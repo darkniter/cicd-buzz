997639e3c8d4623d4520af9f4cc2c7645de8795e
darkniter <Poranot@yandex.ru>
2019-08-19 13:27:44 +0300

    fix ...again

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
 @app.route("/")
 def generate_buzz():
     buzz = generator.generate_buzz()
-    return render_template('main.html', buzz=buzz)
+    return render_template('index.html', buzz=buzz)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=os.getenv('PORT'))
